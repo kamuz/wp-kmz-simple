e17afae23d1452014baa86b0ab32ba86ad99026f
Vladimir Kamuz <v.kamuz@gmail.com>
2019-01-21 14:08:12 +0200

    File header plugin

---
+++ kmz-simple.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Plugin Name: KMZ Simple Example Plugin
+ * Description: Welcome to WordPress Plugin Development
+ * Version: 1.0.0
+ * Author: Vladimir Kamuz
+ * License: GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Author URI: https://wpdev.pp.ua/
+ * Plugin URI: https://github.com/kamuz/kmz-simple
+ * Text Domain: kmz-simple
+ * Domain Path: /languages
+*/

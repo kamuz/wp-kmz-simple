b693f97e0cb5dbbfc7820b6840fb918291bbd73d
Vladimir Kamuz <v.kamuz@gmail.com>
2019-01-21 14:27:42 +0200

    Change plugin URI

--- kmz-simple.php
+++ kmz-simple.php
@@ -8,7 +8,7 @@
  * License: GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  * Author URI: https://wpdev.pp.ua/
- * Plugin URI: https://github.com/kamuz/kmz-simple
+ * Plugin URI: https://github.com/kamuz/wp-kmz-simple
  * Text Domain: kmz-simple
  * Domain Path: /languages
 */
